8d9e66047d2dcd533e38052c9050f91df34c071a
Giovanny A <gaarbelaez@gmail.com>
2018-06-12 13:54:50 +0200

    test with template

---
+++ _posts/2017-06-20-intro-to-research.markdown
@@ -0,0 +1,13 @@
+---
+layout: single
+title:  "Visualisation test of 3D model"
+date:   2017-06-20 11:53:24 +0200
+
+---
+<!-- This post presents a research test. header:
+  image: /assets/images/header.jpg -->
+
+This post presents a test for a 3D model visualisation using p3d.in iframe. The example used the Agisoft doll example data frame.
+
+<iframe allowfullscreen webkitallowfullscreen width="640" height="480" frameborder="0" seamless src="https://p3d.in/e/tyN1p"></iframe>
+
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,9 @@
 var store = [{
+        "title": "Visualisation test of 3D model",
+        "excerpt":"This post presents a test for a 3D model visualisation using p3d.in iframe. The example used the Agisoft doll example data frame. ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/intro-to-research/",
+        "teaser":null},{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
