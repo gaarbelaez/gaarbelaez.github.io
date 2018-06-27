ed8e1ad1aa355edae67a105ac515a7ba521be0e7
Giovanny A <gaarbelaez@gmail.com>
2018-06-27 10:38:25 +0200

    test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,11 @@
 var store = [{
-        "title": "Visualisation test of 3D model",
-        "excerpt":"This post presents a test for a 3D model visualisation using p3d.in iframe. The example used the Agisoft doll example data frame. ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/2017/06/20/intro-to-research.html",
-        "teaser":null},{
         "title": "Welcome",
         "excerpt":"Hi, this is my personal site. Here you will find some info about me, my research and projects but from a personal perspective. ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/2018/06/11/welcome-to-jekyll.html",
+        "teaser":null},{
+        "title": "Visualisation test of 3D model",
+        "excerpt":"This post presents a test for a 3D model visualisation using p3d.in iframe. The example used the Agisoft PhotoScan doll example data frame. We have been recently testing some digitalization technologies for museum and cultural heritage objects using either digital scanners like the ArtecEva or photogrammetry technologies like the Agisoft...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2018/06/26/intro-to-research.html",
         "teaser":null}]
